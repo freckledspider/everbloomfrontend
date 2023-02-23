7f578aa11b7c286d788f87f2982fd91d433c7473
bri <bcamesi@gmail.com>
2023-02-23 16:12:22 -0500

    added inventory data

---
+++ src/inventory.js
@@ -0,0 +1,52 @@
+const inventory = [
+    {
+        "id": 1,
+        "product": "Peachy",
+        "image": "https://i.imgur.com/AQlwLTQ.jpg",
+        "price": 75,
+        "description": "In green and white with a pop of peach, this design draws inspiration from the natural shapes of the flowers, which highlights the organic lines created by their stems and celebrates the carefree placement of their blooms.",
+        "card": ""
+    },
+    {
+        "id": 2,
+        "product": "Verdant Vibes",
+        "image": "https://i.imgur.com/NB7wriz.jpg",
+        "price": 125,
+        "description": "In a palette celebrating all of the new growth, tiny sprouts, and unfurling leaves following the final frost, this lush collection of blooms combines some of our spring faves with roses, hydrangea, and greens bubbling out of a white cylinder vase.",
+        "card": ""
+    },
+    {
+        "id": 3,
+        "product": "Vernal View",
+        "image": "https://i.imgur.com/5uik57c.jpg",
+        "price": 110,
+        "description": "Layers of crisp white and green blooms dance with natural textures, blush roses and softly arching foliage in this garden-inspired design, overflowing from a concrete pot.",
+        "card": ""
+    },
+    {
+        "id": 4,
+        "product": "Denim",
+        "image": "https://i.imgur.com/LNaguxe.jpg",
+        "price": 65,
+        "description": "This captivating arrangement is bound to delight and amaze! Striking blue hydrangea artfully arranged with delphinium, thistle, queen anne's lace, and greenery in a glass cylinder will surely touch their heart today.",
+        "card": ""
+    },
+    {
+        "id": 5,
+        "product": "Spring Elegance",
+        "image": "https://i.imgur.com/3BKRNgC.jpg",
+        "price": 300,
+        "description": "Elegant spring blooms arranged beautifully in a classic style.",
+        "card": ""
+    },
+    {
+        "id": 6,
+        "product": "Pastel Dreams",
+        "image": "https://i.imgur.com/3BKRNgC.jpg",
+        "price": 350,
+        "description": "A lovely collection of deluxe blooms, this arrangement is guaranteed to impress. Pastel roses, ranunculus, cymbidium orchids, eucalyptus, hydrangea, garden roses, stock, and other gorgeous flowers in a sleek glass cylinder.",
+        "card": ""
+    },
+]
+
+export default inventory
