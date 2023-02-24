b8fc695d0fd1b68493bcf474d050706361100a16
bri <bcamesi@gmail.com>
2023-02-24 15:42:30 -0500

    updated best sellers list

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -5,7 +5,9 @@ const Index = (props) => {
   return <div className="index">
     <h1>Best Sellers</h1>
     <Link to={`/inventory/${inventory[2].id}`}><h2>{inventory[2].product}</h2></Link><br/>
-  <img src={inventory[2].image} alt={inventory[2].description} width="200" />
+  <img src={inventory[2].image} alt={inventory[2].description} width="200" /><br/>
+  <Link to={`/inventory/${inventory[5].id}`}><h2>{inventory[5].product}</h2></Link><br/>
+  <img src={inventory[5].image} alt={inventory[5].description} width="200" />
   </div>
 };
 
