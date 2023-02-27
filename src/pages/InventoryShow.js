c166f906e8aa2bc7aceafd8018cc4c63be38a596
bri <bcamesi@gmail.com>
2023-02-26 19:14:01 -0500

    inventoryshow page styled

--- src/pages/InventoryShow.js
+++ src/pages/InventoryShow.js
@@ -1,13 +1,29 @@
 import inventory from "../inventory"
 import { useParams } from "react-router-dom"
+import { Form } from "react-router-dom"
 
 const InventoryShow = (props, params) => {
     const {id} = useParams()
-    return ( <div>
+    return ( <div className="inventoryShow">
+    <table>
+    <tr>
+    <td>
+    <img src={inventory[id].image} alt={inventory[id].description} width="500" /></td>
+    <td>
     <h2>{inventory[id].product}</h2>
-    <img src={inventory[id].image} alt={inventory[id].description} width="400" />
-    <h3>{inventory[id].price}</h3>
-    {inventory[id].description}
+    <h3>${inventory[id].price}</h3>
+    <hr></hr><br/>
+    {inventory[id].description}<br/>
+    <Form action="/create" method="post">
+<input className="displayNone" type="text" name="product" value={inventory[id].product}/>
+<input className="displayNone" type="text" name="image" value={inventory[id].image} />
+<input className="displayNone" type="text" name="price" value={inventory[id].price}/>
+<input className="displayNone" type="text" name="description" value={inventory[id].description}/><br/><br/>
+Enter optional card message: <input type="text" name="card" value="No card message"/><br/><br/>
+<button>Add to Cart</button>
+</Form></td>
+    </tr>
+    </table>
     </div>
     )}
 
