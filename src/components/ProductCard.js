615a045e6c7969cd9358ed4dd5194891c396d905
bri <bcamesi@gmail.com>
2023-02-26 19:34:14 -0500

    shopping cart styled

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,13 +1,25 @@
 import { Link } from "react-router-dom";
+import { Form } from "react-router-dom";
+
 // Shows product cards from database in shopping cart
 const ProductCard = ({ post }) => {
   return (
-    <div>
-      <Link to={`/product/${post.id}`}>
-        <h1>{post.product}</h1>
+    <div id="productCard">
+      <table>
+      <tr>
+      <td><Link to={`/product/${post.id}`}>
+        <h3>{post.product}</h3>
       </Link>
-      <img src={post.image} alt="" width="200" />
-      <h2>{post.price}</h2>
+      <Link to={`/product/${post.id}`}><img src={post.image} alt="" width="200" /></Link></td>
+      <td><h3>${post.price}</h3>
+      Card Message: <i>"{post.card}"</i><br/>
+      <Link to={`/product/${post.id}`}>Edit Card Message</Link><br/><br/>
+      <Form action={`/delete/${post.id}`} method="post">
+        <button>Remove from cart</button>
+      </Form></td>
+      </tr>
+      </table>
+      <hr></hr>
     </div>
   );
 };
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -3,9 +3,12 @@ import {useLoaderData} from "react-router-dom"
 
 const Cart = (props) => {
   const products = useLoaderData()
-  return <>
+  return <div className="cart">
+  <table>
+  <h2>Shopping Cart</h2>
   {products.map((product) => <ProductCard post={product} key={product.id} />)}
-  </>;
+  </table>
+  </div>
 };
 
 export default Cart;
