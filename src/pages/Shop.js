63d8ea5b14c659c39092b1f5482baa13802859f5
bri <bcamesi@gmail.com>
2023-02-26 19:22:18 -0500

    styled shop page

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -4,25 +4,26 @@ import inventory from "../inventory";
 
 const Shop = (props) => {
 
-    return <div>
-    <h2>View Products</h2>
+    return <div className="shop">
+    <table>
+    <h2>Arrangements</h2>
     {inventory.map((flowers) => {
     const { product, price, image, description, id } = flowers;
-    return ( <div>
-    <Link to={`/inventory/${id}`}><h2>{product}</h2></Link>
-    <img src={image} alt="" width="200" /><h3>{price}</h3>
+    return ( <div className="shopCards">
+    <Link to={`/inventory/${id}`}><h3>{product}</h3></Link>
+    <Link to={`/inventory/${id}`}><img src={image} alt={description} width="200" /></Link><h3>${price}</h3>
 <Form action="/create" method="post">
-<input type="text" name="product" value={product}/>
-<input type="text" name="image" value={image} />
-<input type="text" name="price" value={price}/>
-<input type="text" name="description" value={description}/>
-<input type="text" name="card" value="No card message"/>
+<input className="displayNone" type="text" name="product" value={product}/>
+<input className="displayNone" type="text" name="image" value={image} />
+<input  className="displayNone" type="text" name="price" value={price}/>
+<input className="displayNone" type="text" name="description" value={description}/>
+<input className="displayNone" type="text" name="card" value="No card message"/>
 <button>Add to Cart</button>
 </Form>
-                </div>
-                );
-            })}
-  </div>
+            </div>
+            );
+        })}
+  </table></div>
 };
 
 export default Shop;
